9bc41e132fc8f598efd43ac4b1cdc9926267dc88
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-16 02:17:57 +0000

    Import of SHARYANTO/Business-ID-NIK-0.05 from CPAN.
    gitpan-cpan-distribution: Business-ID-NIK
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SHARYANTO/Business-ID-NIK-0.05.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Business/ID/NIK.pm
+++ lib/Business/ID/NIK.pm
@@ -12,7 +12,7 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(parse_nik);
 
-our $VERSION = '0.04'; # VERSION
+our $VERSION = '0.05'; # VERSION
 
 our %SPEC;
 
@@ -101,7 +101,7 @@ __END__
 
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -109,7 +109,7 @@ Business::ID::NIK - Validate Indonesian citizenship registration number (NIK)
 
 =head1 VERSION
 
-version 0.04
+This document describes version 0.05 of Business::ID::NIK (from Perl distribution Business-ID-NIK), released on 2014-08-16.
 
 =head1 SYNOPSIS
 
@@ -134,23 +134,12 @@ nowadays not always [citation needed] composed as: pp 2-digit province code, DD
 code), ddmmyy is date of birth of the citizen (dd will be added by 40 for
 female), ssss is 4-digit serial starting from 1.
 
-=head1 AUTHOR
-
-Steven Haryanto <stevenharyanto@gmail.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2013 by Steven Haryanto.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
 =head1 FUNCTIONS
 
 
-None are exported by default, but they are exportable.
-
 =head2 parse_nik(%args) -> [status, msg, result, meta]
+
+Parse Indonesian citizenship registration number (NIK).
 
 Arguments ('*' denotes required arguments):
 
@@ -172,6 +161,42 @@ Input NIK to be validated.
 
 Return value:
 
-Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+ (any)
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Business-ID-NIK>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Business-ID-NIK>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Business-ID-NIK>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
